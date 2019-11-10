feabae0b2fa47aff52f70f6a2c3eeb61c7642c1c
Toby Clemson <tobyclemson@gmail.com>
2019-11-10 20:00:43 +0000

    Clean up formula.

--- gorgon.rb
+++ gorgon.rb
@@ -1,14 +1,13 @@
 class Gorgon < Formula
   desc "Manage GitHub organizations or users."
   homepage "https://github.com/tobyclemson/gorgon"
-  url "https://github.com/tobyclemson/gorgon/archive/0.2.0.tar.gz"
-  sha256 "4ac358a12aedf6d908db9748c890aec952493f3312e5b040c2b80524c3b77b80"
+  url "https://github.com/tobyclemson/gorgon/archive/0.3.0.tar.gz"
+  sha256 "54852b76ffbe50994dcd663a8cbb75d4f4a995da41e6ffa6309e30bdcab71032"
 
   depends_on "goenv" => [:build, "HEAD"]
   depends_on "rbenv" => :build
   depends_on "ruby-build" => :build
   depends_on "openssl" => :build
-  depends_on "tree" => :build
 
   def install
     File.open("build.sh", "w") do |f|
@@ -20,8 +19,6 @@ class Gorgon < Formula
           
           export GOENV_ROOT="$HOME/.goenv"
           export PATH="$GOENV_ROOT/bin:$PATH"
-          
-          goenv init -
           eval "$(goenv init -)"
 
           goenv install
@@ -32,8 +29,6 @@ class Gorgon < Formula
           export PATH="$GOROOT/bin:$GOPATH/bin:$PATH"
     
           export PATH="$HOME/.rbenv/bin:$PATH"
-
-          rbenv init - bash
           eval "$(rbenv init - bash)"
 
           export RUBY_CONFIGURE_OPTS="--with-openssl-dir=#{prefix}/openssl"
@@ -43,16 +38,7 @@ class Gorgon < Formula
           gem install bundler
           bundle install
           
-          env | sort
-          tree -L 4 -a .brew_home
-          ls -la
-
-          set +e
-          rake 'cli:build[0.2.0]'
-          set -e
-
-          tree -L 4 -a .brew_home
-          ls -la
+          rake 'cli:build:darwin[#{version}]'
       EOS
       f.write(content)
     end
@@ -61,7 +47,7 @@ class Gorgon < Formula
 
     system "./build.sh"
 
-    bin.install "build/bin/0.2.0_darwin_amd64/gorgon"
+    bin.install "build/bin/#{version}_darwin_amd64/gorgon"
   end
 
   test do
